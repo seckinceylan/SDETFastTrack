0f5906c17ec0bfe4a38c398c41a7828ac0357382
Aysun120 <aysundamar114@gmail.com>
2021-12-14 20:37:34 -0600

    RadioButtonPage added

--- src/test/java/com/sdetfasttrack/pages/RadioButtonPage.java
+++ src/test/java/com/sdetfasttrack/pages/RadioButtonPage.java
@@ -14,9 +14,27 @@ public class RadioButtonPage {
     //3. Confirm radio button #Football is NOT selected by default
     //4. Click radio button #Football to select it.
     //5. Verify radio button #Football is selected.
-    //8. Click radio button #Red to select it.
-    //6. Verify radio button #Red is selected.
-    //6. Verify radio button #Blue is not selected.
+    //6. Click radio button #Red to select it.
+    //7. Verify radio button #Red is selected.
+    //8. Verify radio button #Blue is not selected.
+    //9. Verify radio button #Green is not Enabled.
+
+     public RadioButtonPage(){
+         PageFactory.initElements(Driver.getDriver(),this);
+     }
+
+     @FindBy(xpath="//input[@id='blue']")
+     public WebElement blueBtn;
+
+     @FindBy(id="football")
+    public WebElement footballBtn;
+
+     @FindBy(id="red")
+    public WebElement redBtn;
+
+     @FindBy(id="green")
+    public WebElement greenBtn;
+
 
 
 }
--- src/test/java/com/sdetfasttrack/tests/RadioButtonsTest.java
+++ src/test/java/com/sdetfasttrack/tests/RadioButtonsTest.java
@@ -4,10 +4,34 @@ import com.sdetfasttrack.pages.RadioButtonPage;
 import com.sdetfasttrack.utilities.Driver;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class RadioButtonsTest {
 
+     RadioButtonPage radioButtonPage =new RadioButtonPage();
+
+    @Test
+    public void radioButtonTest(){
+        //TC #1:radio buttons
+        //1.GO TO: http://practice.cydeo.com/radio_buttons
+        Driver.getDriver().get("http://practice.cydeo.com/radio_buttons");
+        //2. Confirm radio button #Blue is selected by default
+        assertTrue(radioButtonPage.blueBtn.isSelected(),"Blue button is not selected by default!");
+        //3. Confirm radio button #Football is NOT selected by default
+        assertTrue(!radioButtonPage.footballBtn.isSelected(),"Football button is selected");
+        //assertFalse(radioButtonPage.footballBtn.isSelected(),"Football button is selected");
+        //4. Click radio button #Football to select it.
+        radioButtonPage.footballBtn.click();
+        //5. Verify radio button #Football is selected.
+        assertTrue(radioButtonPage.footballBtn.isSelected(),"Football button is not selected");
+        //6. Click radio button #Red to select it.
+        radioButtonPage.redBtn.click();
+        //7. Verify radio button #Red is selected.
+        assertTrue(radioButtonPage.redBtn.isSelected(),"Red button is not selected");
+        //8. Verify radio button #Blue is not selected.
+        assertTrue(!radioButtonPage.blueBtn.isSelected(),"Blue button is selected");
+        //9. Verify radio button #Green is not Enabled.
+         assertTrue(!radioButtonPage.greenBtn.isEnabled(),"Green button is enabled");
+    }
 
 }
